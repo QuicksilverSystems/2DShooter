bd5805dc3e25d66647f30c44a6279d9ade4a00b4
benjaminbbonnell@gmail.com <benjaminbbonnell@gmail.com>
2014-07-19 15:26:48 -0400

    Basic Setup
    Added a few things to the game

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+	
+	public float characterSpeed = 1f;
+	public string horizontalAxisName = "Horizontal";
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.position += transform.right*Input.GetAxis(horizontalAxisName)*characterSpeed*Time.deltaTime;
+		
+	}
+}
